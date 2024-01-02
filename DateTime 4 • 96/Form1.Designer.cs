c2d95db9f636138b66cf22aab56fa438a73470e4
halley <130781977+mshelovee@users.noreply.github.com>
2024-01-02 10:34:47 +0300

    Add files via upload

---
+++ DateTime 4 • 96/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace DateTime_4___96
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.lblBugun = new System.Windows.Forms.Label();
+            this.lblSonOdeme = new System.Windows.Forms.Label();
+            this.lblGun = new System.Windows.Forms.Label();
+            this.lblSaat = new System.Windows.Forms.Label();
+            this.lblDakika = new System.Windows.Forms.Label();
+            this.lblSaniye = new System.Windows.Forms.Label();
+            this.lblMiliSaniye = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnGoster.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGoster.Location = new System.Drawing.Point(15, 12);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(396, 53);
+            this.btnGoster.TabIndex = 0;
+            this.btnGoster.Text = "Göster";
+            this.btnGoster.UseVisualStyleBackColor = false;
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+            // 
+            // lblBugun
+            // 
+            this.lblBugun.AutoSize = true;
+            this.lblBugun.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBugun.Location = new System.Drawing.Point(12, 100);
+            this.lblBugun.Name = "lblBugun";
+            this.lblBugun.Size = new System.Drawing.Size(44, 16);
+            this.lblBugun.TabIndex = 1;
+            this.lblBugun.Text = "label1";
+            // 
+            // lblSonOdeme
+            // 
+            this.lblSonOdeme.AutoSize = true;
+            this.lblSonOdeme.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonOdeme.Location = new System.Drawing.Point(12, 139);
+            this.lblSonOdeme.Name = "lblSonOdeme";
+            this.lblSonOdeme.Size = new System.Drawing.Size(44, 16);
+            this.lblSonOdeme.TabIndex = 2;
+            this.lblSonOdeme.Text = "label2";
+            // 
+            // lblGun
+            // 
+            this.lblGun.AutoSize = true;
+            this.lblGun.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblGun.Location = new System.Drawing.Point(12, 184);
+            this.lblGun.Name = "lblGun";
+            this.lblGun.Size = new System.Drawing.Size(44, 16);
+            this.lblGun.TabIndex = 3;
+            this.lblGun.Text = "label3";
+            // 
+            // lblSaat
+            // 
+            this.lblSaat.AutoSize = true;
+            this.lblSaat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSaat.Location = new System.Drawing.Point(12, 224);
+            this.lblSaat.Name = "lblSaat";
+            this.lblSaat.Size = new System.Drawing.Size(44, 16);
+            this.lblSaat.TabIndex = 4;
+            this.lblSaat.Text = "label4";
+            // 
+            // lblDakika
+            // 
+            this.lblDakika.AutoSize = true;
+            this.lblDakika.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDakika.Location = new System.Drawing.Point(12, 266);
+            this.lblDakika.Name = "lblDakika";
+            this.lblDakika.Size = new System.Drawing.Size(44, 16);
+            this.lblDakika.TabIndex = 5;
+            this.lblDakika.Text = "label5";
+            // 
+            // lblSaniye
+            // 
+            this.lblSaniye.AutoSize = true;
+            this.lblSaniye.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSaniye.Location = new System.Drawing.Point(12, 309);
+            this.lblSaniye.Name = "lblSaniye";
+            this.lblSaniye.Size = new System.Drawing.Size(44, 16);
+            this.lblSaniye.TabIndex = 6;
+            this.lblSaniye.Text = "label6";
+            // 
+            // lblMiliSaniye
+            // 
+            this.lblMiliSaniye.AutoSize = true;
+            this.lblMiliSaniye.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMiliSaniye.Location = new System.Drawing.Point(12, 349);
+            this.lblMiliSaniye.Name = "lblMiliSaniye";
+            this.lblMiliSaniye.Size = new System.Drawing.Size(44, 16);
+            this.lblMiliSaniye.TabIndex = 7;
+            this.lblMiliSaniye.Text = "label7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(423, 450);
+            this.Controls.Add(this.lblMiliSaniye);
+            this.Controls.Add(this.lblSaniye);
+            this.Controls.Add(this.lblDakika);
+            this.Controls.Add(this.lblSaat);
+            this.Controls.Add(this.lblGun);
+            this.Controls.Add(this.lblSonOdeme);
+            this.Controls.Add(this.lblBugun);
+            this.Controls.Add(this.btnGoster);
+            this.Name = "Form1";
+            this.Text = "41";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGoster;
+        private System.Windows.Forms.Label lblBugun;
+        private System.Windows.Forms.Label lblSonOdeme;
+        private System.Windows.Forms.Label lblGun;
+        private System.Windows.Forms.Label lblSaat;
+        private System.Windows.Forms.Label lblDakika;
+        private System.Windows.Forms.Label lblSaniye;
+        private System.Windows.Forms.Label lblMiliSaniye;
+    }
+}
+
